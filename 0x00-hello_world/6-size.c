a9313c1f01266805397417d4589e028fb2492f93
Sarahsarahb <sarahaharak@gmail.com>
2023-03-16 17:04:24 -0700

    101-quote.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,15 +13,15 @@ long int c;
 long long int d;
 float f;
 
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a char: %d byte(s)\n", (unsigned long)sizeof(a));
 
-printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of an int: %d byte(s)\n", (unsigned long)sizeof(b));
 
-printf("Size of a long Ant: %lu bytes(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long Ant: %d bytes(s)\n", (unsigned long)sizeof(c));
 
-printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
+printf("Size of a long long int: %d bytes(s)\n", (unsigned long)sizeof(d));
 
-printf("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
+printf("Size of a float: %d bytes(s)\n", (unsigned long)sizeof(f));
 
 return (0);
 }
