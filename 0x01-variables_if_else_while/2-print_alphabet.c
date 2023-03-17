879335fe9dbf67d88a5ae4189f43d89334c3c30f
Sarahsarahb <sarahaharak@gmail.com>
2023-03-17 13:51:33 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
+
