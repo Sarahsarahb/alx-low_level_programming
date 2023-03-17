40e159de411f4493b4a0a2fe3b66cd5af55c51af
Sarahsarahb <sarahaharak@gmail.com>
2023-03-17 13:55:09 -0700

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game
+ * main - I sometimes suffer from insomnia. And when I can't fall asleep,
+ * I play what I call the alphabet game
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -15,4 +16,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-
