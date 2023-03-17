c7193beada08ae08f554fe1212f0eb6a1b4edc50
Sarahsarahb <sarahaharak@gmail.com>
2023-03-16 17:06:09 -0700

    101-quote.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,27 +1,15 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- *  * main -  Size is not grandeur, and territory does not make a nation.
- *  * Return: 0 if success.
- */
-
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-
-char a;
-int b;
-long int c;
-long long int d;
-float f;
-
-printf("Size of a char: %d byte(s)\n", (unsigned long)sizeof(a));
-
-printf("Size of an int: %d byte(s)\n", (unsigned long)sizeof(b));
-
-printf("Size of a long Ant: %d bytes(s)\n", (unsigned long)sizeof(c));
-
-printf("Size of a long long int: %d bytes(s)\n", (unsigned long)sizeof(d));
-
-printf("Size of a float: %d bytes(s)\n", (unsigned long)sizeof(f));
-
-return (0);
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
 }
